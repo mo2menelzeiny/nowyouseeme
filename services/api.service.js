a79c3a556abe645201b1c6898fcac16344100490
mo2menelzeiny <mo2menelzeiny@gmail.com>
2020-01-31 06:14:28 +0200

    chore: fix indent

--- services/api.service.js
+++ services/api.service.js
@@ -3,23 +3,23 @@
 const ApiGateway = require("moleculer-web");
 
 module.exports = {
-	name: "api",
-	mixins: [ApiGateway],
-	settings: {
-		port: process.env.PORT || 3000,
+    name: "api",
+    mixins: [ApiGateway],
+    settings: {
+        port: process.env.PORT || 3000,
 
-		routes: [{
-			path: "/api",
-			whitelist: [
-				// Access to any actions in all services under "/api" URL
-				"authorization.generateUserId",
-				"presence.getPresenceTimestamp"
-			]
-		}],
+        routes: [{
+            path: "/api",
+            whitelist: [
+                // Access to any actions in all services under "/api" URL
+                "authorization.generateUserId",
+                "presence.getPresenceTimestamp"
+            ]
+        }],
 
-		// Serve assets from "public" folder
-		assets: {
-			folder: "public"
-		}
-	}
+        // Serve assets from "public" folder
+        assets: {
+            folder: "public"
+        }
+    }
 };
